7803bf1aa29f9cb1db7e29b95874c1b9b8617832
Mark Sayson <masayson@gmail.com>
2024-06-19 22:02:17 -0700

    Remove snapshot dependency on specific service code version
    Update the API stack template tests to strip or replace
    template sections which change every time the upstream
    API service code is updated, so that the tests can run
    consistently/reliably regardless of specific API service
    code, and validate the CDK-side infrastructure changes.

--- test/stacks/ConsentManagementApiStack.test.ts
+++ test/stacks/ConsentManagementApiStack.test.ts
@@ -7,6 +7,20 @@ import { MOCK_STAGE_CONFIG } from '../../fixtures/mock-stage-config';
 import { ConsentManagementApiStack } from '../../lib/stacks/ConsentManagementApiStack';
 
 describe('ConsentManagementApiStack', () => {
+  // Strip or replace template sections that change on every upstream API service code update
+  function cleanApiResources(templateJson: any) {
+    // Clear API service code S3 references which change every time there are upstream code updates
+    delete templateJson['Resources']['ConsentManagementAPILambda33B42EFA']['Properties']['Code'];
+
+    // Replace resource keys which change every time there are upstream service code updates
+    for (const resourceKey in templateJson['Resources']) {
+      if (resourceKey.startsWith('ConsentManagementAPILambdaInvoke')) {
+        templateJson['Resources']['ConsentManagementAPILambdaInvoke'] = templateJson['Resources'][resourceKey];
+        delete templateJson['Resources'][resourceKey];
+      }
+    }
+  }
+
   it('creates the expected CloudFormation template from CDK', () => {
     const app = new App();
     const consentDataStack = new ConsentManagementApiStack(app, 'ConsentManagementApiStack', {
@@ -14,7 +28,10 @@ describe('ConsentManagementApiStack', () => {
       stageConfig: MOCK_STAGE_CONFIG,
       apiCodePackageFilePath: join(__dirname, '../../../consent-management-api')
     });
-    const template = Template.fromStack(consentDataStack);
-    expect(template.toJSON()).toMatchSnapshot();
-  })
+
+    const templateJson = Template.fromStack(consentDataStack).toJSON();
+    cleanApiResources(templateJson);
+
+    expect(templateJson).toMatchSnapshot();
+  });
 });
