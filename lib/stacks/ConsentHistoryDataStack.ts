1549bb69c46d029ce9d9bc14309c4f5ac3251834
Mark Sayson <masayson@gmail.com>
2024-07-04 22:38:19 -0700

    Added consent history data stack

---
+++ lib/stacks/ConsentHistoryDataStack.ts
@@ -0,0 +1,40 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table, TableEncryption } from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+
+import { StageConfig } from '../interfaces/stage-config';
+
+export interface ConsentHistoryDataStackProps extends StackProps {
+  stageConfig: StageConfig;
+}
+
+/**
+ * Defines the consent history data storage layer.
+ */
+export class ConsentHistoryDataStack extends Stack {
+  public readonly consentTable: Table;
+
+  constructor(scope: Construct, id: string, readonly props: ConsentHistoryDataStackProps) {
+    super(scope, id, props);
+
+    this.consentTable = this.createConsentHistoryTable();
+  }
+
+  private createConsentHistoryTable() {
+    return new Table(this, 'ServiceUserConsentHistoryDynamoDBTable', {
+      tableName: 'ConsentHistory',
+      encryption: TableEncryption.AWS_MANAGED,
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      sortKey: {
+        name: 'eventId',
+        type: AttributeType.STRING
+      },
+      pointInTimeRecovery: true,
+      deletionProtection: true,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+  }
+}
