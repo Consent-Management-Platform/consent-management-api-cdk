99e1147b40c14680b8f69fa0f325ee86b078a4f5
Mark Sayson <masayson@gmail.com>
2025-03-22 23:06:03 -0700

    Add warning comments for changing export names depended on by integ tests

--- lib/constants/apis.ts
+++ lib/constants/apis.ts
@@ -1,8 +1,16 @@
+// ***************************
 // API CloudFormation exports
+// ***************************
 export const CONSENT_MANAGEMENT_API_ENDPOINT_EXPORT_NAME = 'ConsentManagementApiEndpoint';
+
+// WARNING: Do not change this export name without also updating the integ tests in .github/actions/.
+// The integ tests rely on the export name to dynamically retrieve the API Gateway ID and resource IDs,
+// which they use to make API Gateway TestInvokeMethod CLI calls to query the API endpoints.
 export const CONSENT_MANAGEMENT_API_GATEWAY_ID_EXPORT_NAME = 'ConsentManagementApiGatewayId';
 
+// ***************************
 // API Gateway properties
+// ***************************
 export const CONSENT_MANAGEMENT_API_DOCS_URL = 'https://consent-management-platform.github.io/consent-management-api-models/v1/docs.html';
 
 // Restrict the maximum number of active and queued requests per second
