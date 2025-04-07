ff8e8e1ef771a26d30f9c75813d7234cffda0972
Mark Sayson <masayson@gmail.com>
2025-04-06 22:19:12 -0700

    Add ConsentsByServiceUser DDB GSI and update ConsentHistory API specs
    Update the ConsentHistory API OpenAPI spec to reflect the latest
    model changes and update CDK stack test snapshots to include the
    new GetHistoryForServiceUser API endpoint.
    
    Add a ConsentHistoryByServiceUser DynamoDB GSI on the ConsentHistory
    DynamoDB table to support the new API's use case of querying
    for all consent history records associated with a given serviceId
    + userId pair.

--- lib/stacks/ConsentHistoryDataStack.ts
+++ lib/stacks/ConsentHistoryDataStack.ts
@@ -1,5 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { AttributeType, BillingMode, Table, TableEncryption } from 'aws-cdk-lib/aws-dynamodb';
+import { AttributeType, BillingMode, ProjectionType, Table, TableEncryption } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 import { CustomDynamoDbTable } from '../constructs/CustomDynamoDbTable';
@@ -19,8 +19,20 @@ export class ConsentHistoryDataStack extends Stack {
     super(scope, id, props);
 
     this.consentHistoryTable = this.createConsentHistoryTable();
+    this.createServiceUserGsi(this.consentHistoryTable);
   }
 
+  /**
+   * Creates the ConsentHistory DynamoDB table.
+   *
+   * Every ConsentHistory record is associated with an id partition key + eventId
+   * sort key pair, where id is constructed from "{serviceId}|{userId}|{consentId}",
+   * and eventId is a unique identifier for a given consent change event.
+   *
+   * Table queries must specify the partition key, while the sort key is optional.
+   * Querying the table with id = "{serviceId}|{userId}|{consentId}", without specifying
+   * eventId, will return all ConsentHistory records for the given consent.
+   */
   private createConsentHistoryTable() {
     return new CustomDynamoDbTable(this, 'ServiceUserConsentHistoryDynamoDBTable', {
       tableName: 'ConsentHistory',
@@ -32,6 +44,35 @@ export class ConsentHistoryDataStack extends Stack {
         name: 'eventId',
         type: AttributeType.STRING
       },
+    });
+  }
+
+  /**
+   * Creates a ConsentHistoryByServiceUser index on the consent history table.
+   *
+   * This DynamoDB Global Secondary Index (GSI) allows querying for the history of
+   * all consents associated with a serviceId + userId pair.
+   *
+   * Each GSI partition key + sort key pair must be unique in the table, while there can
+   * be multiple items with the same partition key.  Every GSI query must specify the
+   * partition key, while including the sort key in the query request is optional.
+   *
+   * By querying the ConsentHistoryByServiceUser GSI with serviceUserId =
+   * "{serviceId}|{userId}", without specifying eventId, we can retrieve all
+   * ConsentHistory records for the given serviceId + userId pair.
+   */
+  private createServiceUserGsi(consentHistoryTable: Table) {
+    consentHistoryTable.addGlobalSecondaryIndex({
+      indexName: 'ConsentHistoryByServiceUser',
+      partitionKey: {
+        name: 'serviceUserId',
+        type: AttributeType.STRING
+      },
+      sortKey: {
+        name: 'eventId',
+        type: AttributeType.STRING
+      },
+      projectionType: ProjectionType.ALL
     });
   }
 }
