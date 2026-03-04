a205584d5336e2dff5584441e16c0a4c5d1c92a3
Mark Sayson <masayson@gmail.com>
2026-03-03 22:54:12 -0800

    Update AWS CDK dependencies
    Update AWS CDK dependencies to bring in
    security and bug fixes.

--- test/stacks/CodePipelineStack.test.ts
+++ test/stacks/CodePipelineStack.test.ts
@@ -6,13 +6,25 @@ import { MOCK_STAGE_CONFIG } from '../../fixtures/mock-stage-config';
 import { CodePipelineStack } from '../../lib/stacks/CodePipelineStack';
 
 describe('CodePipelineStack', () => {
+  // Normalize template attributes that change based on upstream package updates
+  function cleanDynamicResources(templateJson: any) {
+    const PROPERTIES_KEY = 'Properties';
+    const RESOURCES_KEY = 'Resources';
+
+    // Remove code path/hash references which change with upstream updates
+    delete templateJson[RESOURCES_KEY]['CustomAWSCDKOpenIdConnectProviderCustomResourceProviderHandlerF2C543E0'][PROPERTIES_KEY]['Code'];
+    delete templateJson[RESOURCES_KEY]['GitHubOIDCProvider4F328116'][PROPERTIES_KEY]['CodeHash'];
+  }
+
   it('creates the expected CloudFormation template from CDK', () => {
     const app = new App();
-    const stack = new CodePipelineStack(app, 'CodePipelineStack', {
+    const codePipelineStack = new CodePipelineStack(app, 'CodePipelineStack', {
       env: MOCK_ENV,
       stageConfig: MOCK_STAGE_CONFIG
     });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+    const templateJson = Template.fromStack(codePipelineStack).toJSON();
+    cleanDynamicResources(templateJson);
+
+    expect(templateJson).toMatchSnapshot();
   })
 });
